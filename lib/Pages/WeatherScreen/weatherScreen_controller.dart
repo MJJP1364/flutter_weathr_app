7f373dde5c41ad8ec039a4a9eeabcd1951828724
m.javad javadpoor <m.j.j.p1364@gmail.com>
2025-05-30 14:55:26 +0330

    some change

--- lib/Pages/WeatherScreen/weatherScreen_controller.dart
+++ lib/Pages/WeatherScreen/weatherScreen_controller.dart
@@ -1,127 +1,3 @@
-// import 'package:flutter/widgets.dart';
-// import 'package:get/get.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-
-// const String apikey = '7eba17b6d1974e6fa91163305241804';
-
-// class WeatherscreenController extends GetxController {
-
-//   @override
-//   void onInit() {
-//      getCurrentWeather(location.value);
-//      getHourlyForcast(location.value);
-//       getPast7DayWeather(location.value).then((value) {
-//         past7DayWeather.value = value;
-//       });
-//     super.onInit();
-//   }
-
-//   final String _baseUrl = 'https://www.weatherapi.com/v1';
-
-//   RxString location = 'Tabriz'.obs;
-//   RxString country = 'Iran'.obs;
-//   RxString currentTemp = '0'.obs;
-//   RxMap<String, dynamic> currentWeather = <String, dynamic>{}.obs;
-//   RxList<dynamic> hourly = <dynamic>[].obs;
-//   RxList<dynamic> past7DayWeather = <dynamic>[].obs;
-//   RxList<dynamic> next7DayWeather = <dynamic>[].obs;
-//   RxBool isLoading = false.obs;
-
-//   TextEditingController searchController = TextEditingController();
-
-//   Future<Map<String, dynamic>> getCurrentWeather(String location) async {
-//     isLoading.value = true;
-//     var data = <String, dynamic>{};
-//     final url = Uri.parse(
-//       '$_baseUrl/current.json?key=$apikey&q=$location',
-//     );
-//     try {
-//       final response = await http.get(url);
-//       if (response.statusCode == 200) {
-//         isLoading.value = false;
-//         if (GetUtils.isNullOrBlank(response.body) == true) {
-//           return {};
-//         } else {
-//           data = Map<String, dynamic>.from(jsonDecode(response.body));
-//           currentWeather.value = data['current'];
-//           currentTemp.value = data['current']['temp_c'].toString();
-//           location = data['location']['name'].obs;
-//           country = data['location']['country'].obs;
-//           hourly.value = data['forecast']['forecastday'][0]['hour'];
-//           next7DayWeather.value = data['forecast']['forecastday'];
-//           past7DayWeather.value = await getPast7DayWeather(this.location.value);
-//         }
-//       } else {
-//         isLoading.value = false;
-//         throw Exception('Failed to load weather data');
-//       }
-//     } catch (e) {
-//       isLoading.value = false;
-//       throw Exception('Error fetching weather data: $e');
-//     }
-//     return data;
-//   }
-
-//   Future<Map<String, dynamic>> getHourlyForcast(String location) async {
-//     isLoading.value = true;
-//     var data = <String, dynamic>{};
-//     final url = Uri.parse(
-//       '$_baseUrl/forecast.json?key=$apikey&q=$location&days=7',
-//     );
-//     try {
-//       final response = await http.get(url);
-//       if (response.statusCode == 200) {
-//         isLoading.value = false;
-//         if (GetUtils.isNullOrBlank(response.body) == true) {
-//           return {};
-//         } else {
-//           data = Map<String, dynamic>.from(jsonDecode(response.body));
-//         }
-//       } else {
-//         isLoading.value = false;
-//         throw Exception('Failed to load weather data');
-//       }
-//     } catch (e) {
-//       isLoading.value = false;
-//       throw Exception('Error fetching weather data: $e');
-//     }
-//     return data;
-//   }
-
-//   Future<List<Map<String, dynamic>>> getPast7DayWeather(String location) async {
-//     final List<Map<String, dynamic>> past7DayWeather = [];
-//     final data = DateTime.now().subtract(Duration(days: 1));
-//     final String formattedDate =
-//         '${data.year}-${data.month.toString().padLeft(2, '0')}-${data.day.toString().padLeft(2, '0')}';
-//     final url = Uri.parse(
-//       '$_baseUrl/history.json?key=$apikey&q=$location&dt=$formattedDate',
-//     );
-
-//     try {
-//       final response = await http.get(url);
-//       if (response.statusCode == 200) {
-//         if (GetUtils.isNullOrBlank(response.body) == true) {
-//           return [];
-//         } else {
-//           final Map<String, dynamic> jsonData = jsonDecode(response.body);
-//           final List<dynamic> forecastDays =
-//               jsonData['forecast']['forecastday'];
-//           for (var day in forecastDays) {
-//             past7DayWeather.add(Map<String, dynamic>.from(day));
-//           }
-//         }
-//       } else {
-//         throw Exception('Failed to load past 7 days weather data');
-//       }
-//     } catch (e) {
-//       throw Exception('Error fetching past 7 days weather data: $e');
-//     }
-
-//     return past7DayWeather;
-//   }
-// }
-
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
@@ -135,10 +11,18 @@ class WeatherscreenController extends GetxController {
   // State Variables
   var location = 'Tabriz'.obs;
   var country = ''.obs;
+  var condition = ''.obs;
+  var iconUrl = ''.obs;
   var currentTemp = '0'.obs;
   var hourly = <dynamic>[].obs;
   var next7DayWeather = <dynamic>[].obs;
   var past7DayWeather = <dynamic>[].obs;
+  RxInt humidity = 0.obs;
+  RxInt windSpeed = 0.obs; // Uncomment if you want to use wind speed
+  RxInt pressure = 0.obs; // Uncomment if you want to use pressure
+  RxString maxTemp = ''.obs;
+  final RxList<dynamic> hourlyForecast = <dynamic>[].obs;
+  RxInt cloud = 0.obs;
 
   // Loading & Error Handling
   var isLoading = false.obs;
@@ -171,6 +55,7 @@ class WeatherscreenController extends GetxController {
         if (locationData != null) {
           location.value = locationData['name'] ?? 'Unknown';
           country.value = locationData['country'] ?? 'Unknown';
+          // condition.value = locationData['condition']['text'] ?? 'Unknown';
         }
 
         // 2️⃣ چک کردن current
@@ -179,6 +64,41 @@ class WeatherscreenController extends GetxController {
           currentTemp.value = currentData['temp_c']?.toString() ?? '0';
           hourly.value = currentData['hour'] as List? ?? [];
         }
+        // 2️⃣ چک کردن condition
+        final conditionData =
+            currentData?['condition'] as Map<String, dynamic>?;
+        if (conditionData != null) {
+          condition.value = conditionData['text'] ?? 'Unknown';
+          iconUrl.value = conditionData['icon'] ?? '';
+        }
+        // 2️⃣ چک کردن iconUrl
+        if (iconUrl.value.isNotEmpty) {
+          iconUrl.value = 'http:${iconUrl.value}';
+        } else {
+          iconUrl.value =
+              'http://cdn.weatherapi.com/weather/64x64/day/113.png'; // Default icon
+        }
+        // 2️⃣ چک کردن humidity
+        humidity.value = currentData?['humidity'] ?? 0;
+        // 2️⃣ چک کردن windSpeed
+        windSpeed.value =
+            currentData?['wind_kph']?.round() ??
+            0; // Uncomment if you want to use wind speed
+        // 2️⃣ چک کردن pressure
+        pressure.value =
+            currentData?['pressure_mb']?.round() ??
+            0; // Uncomment if you want to use pressure
+
+        // 2️⃣ چک کردن cloud
+        cloud.value = currentData?['cloud'] ?? 0;
+
+        // 2️⃣ چک کردن maxTemp
+        maxTemp.value = currentData?['maxtemp_c']?.toString() ?? '0';
+
+        // 2️⃣ چک کردن hourlyForecast
+        final hourlyData = currentData?['hour'] as List? ?? [];
+        hourlyForecast.value =
+            hourlyData.map((hour) => Map<String, dynamic>.from(hour)).toList();
 
         // 3️⃣ چک کردن forecast
         final forecastData = data['forecast'] as Map<String, dynamic>?;
@@ -197,12 +117,12 @@ class WeatherscreenController extends GetxController {
       }
     } catch (e) {
       errorMessage.value = "Failed to load weather data";
-      Get.snackbar(
-        "Error",
-        errorMessage.value,
-        snackPosition: SnackPosition.BOTTOM,
-      );
-      print('Error fetching current weather: $e');
+      // Get.snackbar(
+      //   "Error",
+      //   errorMessage.value,
+      //   snackPosition: SnackPosition.BOTTOM,
+      // );
+      // print('Error fetching current weather: $e');
     } finally {
       isLoading.value = false;
     }
--- lib/Pages/WeatherScreen/weather_screen.dart
+++ lib/Pages/WeatherScreen/weather_screen.dart
@@ -49,17 +49,354 @@ class WeatherScreen extends StatelessWidget {
         }
 
         if (_weatherscreenController.errorMessage.isNotEmpty) {
-          return Text(_weatherscreenController.errorMessage.value);
+          // return Text(_weatherscreenController.errorMessage.value);
+          return Center(
+            child: Text(
+              'Please enter a valid location',
+              style: TextStyle(
+                color: Theme.of(context).textTheme.bodyLarge?.color,
+                fontSize: 25,
+              ),
+            ),
+          );
         }
 
-        return Column(
-          children: [
-            Text('Location: ${_weatherscreenController.location.value}'),
-            Text(
-              'Temperature: ${_weatherscreenController.currentTemp.value} °C',
-            ),
-            // ... other widgets
-          ],
+        return SizedBox(
+          width: size.width,
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                '${_weatherscreenController.location.value} , ${_weatherscreenController.country.value.isNotEmpty ? _weatherscreenController.country : ''}',
+                style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyLarge?.color,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '${_weatherscreenController.currentTemp.value} °C',
+                style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyLarge?.color,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              // ... other widgets
+              // const SizedBox(height: 20),
+              Text(
+                _weatherscreenController.condition.value.isNotEmpty
+                    ? _weatherscreenController.condition.value
+                    : 'No condition data',
+                style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyLarge?.color,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+
+              if (_weatherscreenController.iconUrl.value.isNotEmpty)
+                SizedBox(
+                  width: size.width * 0.8,
+                  child: Image.network(
+                    width: 200,
+                    height: 200,
+                    _weatherscreenController.iconUrl.value,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Icon(
+                        Icons.error,
+                        size: size.width * 0.3,
+                        color: Theme.of(context).colorScheme.error,
+                      );
+                    },
+                  ),
+                ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).colorScheme.shadow,
+                        blurRadius: 10,
+                        offset: const Offset(1, 1),
+                        spreadRadius: 1,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          // for humidity
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Humidity',
+                              style: TextStyle(
+                                color:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodyLarge?.color,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Obx(
+                              () => Text(
+                                '${_weatherscreenController.humidity.value}%',
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).textTheme.bodyLarge?.color,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Image.network(
+                              'https://cdn-icons-png.flaticon.com/512/728/728093.png',
+                              width: 30,
+                              height: 30,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(
+                                  Icons.error,
+                                  size: 30,
+                                  color: Theme.of(context).colorScheme.error,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+
+                        Column(
+                          // for pressure
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Pressure',
+                              style: TextStyle(
+                                color:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodyLarge?.color,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Obx(
+                              () => Text(
+                                '${_weatherscreenController.pressure.value} hPa',
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).textTheme.bodyLarge?.color,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Image.network(
+                              'https://cdn-icons-png.freepik.com/256/4117/4117372.png?uid=R132472831&ga=GA1.1.1207369067.1748603320&semt=ais_incoming',
+                              width: 30,
+                              height: 30,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(
+                                  Icons.error,
+                                  size: 30,
+                                  color: Theme.of(context).colorScheme.error,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        Column(
+                          // for wind speed
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Wind Speed',
+                              style: TextStyle(
+                                color:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodyLarge?.color,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Obx(
+                              () => Text(
+                                '${_weatherscreenController.windSpeed.value} km/h',
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).textTheme.bodyLarge?.color,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Image.network(
+                              'https://cdn-icons-png.flaticon.com/512/5918/5918654.png',
+                              width: 30,
+                              height: 30,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(
+                                  Icons.error,
+                                  size: 30,
+                                  color: Theme.of(context).colorScheme.error,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+
+                        // for cloud
+                        Column(
+                          // for cloud
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Cloud',
+                              style: TextStyle(
+                                color:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.bodyLarge?.color,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Obx(
+                              () => Text(
+                                '${_weatherscreenController.cloud.value}%',
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).textTheme.bodyLarge?.color,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Image.network(
+                              'https://cdn-icons-png.flaticon.com/512/4834/4834559.png',
+                              width: 30,
+                              height: 30,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(
+                                  Icons.error,
+                                  size: 30,
+                                  color: Theme.of(context).colorScheme.error,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+
+                        // for max temprature
+                        // Column(
+                        //   // for max temperature
+                        //   mainAxisAlignment: MainAxisAlignment.center,
+                        //   children: [
+                        //     Text(
+                        //       'Max Temp',
+                        //       style: TextStyle(
+                        //         color:
+                        //             Theme.of(
+                        //               context,
+                        //             ).textTheme.bodyLarge?.color,
+                        //         fontSize: 12,
+                        //       ),
+                        //     ),
+                        //     Obx(
+                        //       () => Text(
+                        //         '${_weatherscreenController.maxTemp.value} °C',
+                        //         style: TextStyle(
+                        //           color:
+                        //               Theme.of(
+                        //                 context,
+                        //               ).textTheme.bodyLarge?.color,
+                        //           fontSize: 15,
+                        //           fontWeight: FontWeight.bold,
+                        //         ),
+                        //       ),
+                        //     ),
+                        //     Image.network(
+                        //       'https://cdn-icons-png.flaticon.com/512/6281/6281340.png',
+                        //       width: 30,
+                        //       height: 30,
+                        //       errorBuilder: (context, error, stackTrace) {
+                        //         return Icon(
+                        //           Icons.error,
+                        //           size: 30,
+                        //           color: Theme.of(context).colorScheme.error,
+                        //         );
+                        //       },
+                        //     ),
+                        //   ],
+                        // ),
+                        // for hourly forecast
+                        // Column(
+                        //   mainAxisAlignment: MainAxisAlignment.center,
+                        //   children: [
+                        //     Text(
+                        //       'Hourly Forecast',
+                        //       style: TextStyle(
+                        //         color:
+                        //             Theme.of(
+                        //               context,
+                        //             ).textTheme.bodyLarge?.color,
+                        //         fontSize: 12,
+                        //       ),
+                        //     ),
+                        //     Obx(
+                        //       () => Text(
+                        //         '${_weatherscreenController.hourlyForecast.length} hours',
+                        //         style: TextStyle(
+                        //           color:
+                        //               Theme.of(
+                        //                 context,
+                        //               ).textTheme.bodyLarge?.color,
+                        //           fontSize: 15,
+                        //           fontWeight: FontWeight.bold,
+                        //         ),
+                        //       ),
+                        //     ),
+                        //     Image.network(
+                        //       'https://cdn-icons-png.flaticon.com/512/1046/1046784.png',
+                        //       width: 30,
+                        //       height: 30,
+                        //       errorBuilder: (context, error, stackTrace) {
+                        //         return Icon(
+                        //           Icons.error,
+                        //           size: 30,
+                        //           color: Theme.of(context).colorScheme.error,
+                        //         );
+                        //       },
+                        //     ),
+                        //   ],
+                        // ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         );
       }),
     );
